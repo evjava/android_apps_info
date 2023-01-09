e2d3fea1c30f4eb647cf2b827f5dab98f97f3579
evjava <evjava@yandex.ru>
2023-01-09 23:31:44 +0300

    master: converted gradle -> gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,19 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 rootProject.name = "AppsInfo"
-include ':app'
+include(":app")
